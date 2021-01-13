abf8ce4879d8594dceae5e92a0d98bcc527ef262
Kristina <kashqar1117@gmail.com>
2021-01-13 15:25:24 -0600

    added content, image links, table of content links

--- Develop/README.md
+++ Develop/README.md
@@ -1,9 +1,40 @@
-# kristina
+# oljfelisjgklsr
+
+ ## Description
+ 
+ kdslkfgjsdkljdls
 
  ## Table of contents
- ### Instalation
- ### Usage Information
- ### Contribution Guidlines
- ### Questions
-  1.Email
+ ### [Instructions for Instalation](#Instalation)
+ ### [usage-information](#usage)
+ ### [Contribution Guidlines](#Contribution)
+ ### [Questions](#questions)
+  1. Email
   2. Github Information
+  
+  ## Instructions for Instalation
+
+  ![Instalation image](instal_image)
+
+  djklsgksdjfskgsdjklfgjskldjfgskl
+
+  ## Usage Information
+
+  ![Usage image](usage_image)
+  
+  lkdjhsfgjkdshfgjkdfhjkghdjkfghjdh
+
+  ## Contribution Guidlines
+
+  skldfjjsdfjksdhgjk
+
+  ## Questions
+
+  Github Username: ndfjkshdjkgfhsjkdhgs
+
+  URL: [Click Here to see my GitHub account](https://github.com/kashqar1117)
+
+  Email: gkjsdhjghdskfjhgs
+
+  To contact me please send me an email from 9am to 5pm Sunday through Saturday.
+  
--- Develop/index.js
+++ Develop/index.js
@@ -4,6 +4,7 @@ let title;
 
 
 
+
 // array of questions for user
 // const questions = [
 // "What is your project title?", 
@@ -26,7 +27,7 @@ function writeToFile(fileName, data) {
             },
             {
                 type: 'input',
-                name: 'Description?',
+                name: 'Description',
                 message: "Write a short description."
             },
             {
@@ -42,33 +43,85 @@ function writeToFile(fileName, data) {
             {
                 type: 'input',
                 name: 'contributionGuidlines',
-                message: "Write out usage information."
+                message: "contributions"
             },
             {
                 type: 'input',
                 name: 'gitHub',
                 message: "Whats your gitHub username?"
+            },
+           
+          
+            {
+                type: 'input',
+                name: 'email',
+                message: "Whats your Email?"
+            },
+            {
+                type: 'input',
+                name: 'instal_image',
+                message: "enter install image url"
+            },
+            {
+                type: 'input',
+                name: 'usage_image',
+                message: "enter usage image url"
             }
+          
 
             
         ])
         .then(answers => {
-      
-
-            //set values in varibles in global
-            title = answers.title
-            console.log(title)
 
 
-            const markdown = `# ${answers.title}
+            const title = answers.title
+            const desc = answers.Description
+            const install = answers.instalation
+            const usage = answers.usageInfromation
+            const contribute = answers.contributionGuidlines
+            const github = answers.gitHub
+            const email = answers.email
+
+            const markdown = `# ${title}
+
+ ## Description
+ 
+ ${desc}
 
  ## Table of contents
- ### Instalation
- ### Usage Information
- ### Contribution Guidlines
- ### Questions
-  1.Email
-  2. Github Information`
+ ### [Instructions for Instalation](#Instalation)
+ ### [Usage Information](#usage)
+ ### [Contribution Guidlines](#Contribution)
+ ### [Questions](#questions)
+  1. Email
+  2. Github Information
+  
+  ## Instructions for Instalation
+
+  ![Instalation image](instal_image)
+
+  ${install}
+
+  ## Usage Information
+
+  ![Usage image](usage_image)
+  
+  ${usage}
+
+  ## Contribution Guidlines
+
+  ${contribute}
+
+  ## Questions
+
+  Github Username: ${github}
+
+  URL: [Click Here to see my GitHub account](https://github.com/kashqar1117)
+
+  Email: ${email}
+
+  To contact me please send me an email from 9am to 5pm Sunday through Saturday.
+  `
 
             fs.writeFile('README.md' , markdown ,(err) => {
                 if (err) throw err;
